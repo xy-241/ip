6d3176d4faeea4085c2ecb3099d73652f727ea02
Xinyang YU <47915643+xy-241@users.noreply.github.com>
2026-08-15 20:47:00 +0800

    Add Level-1: echo input in loop until bye

--- src/main/java/Chin.java
+++ src/main/java/Chin.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Chin {
     private static final String LINE = "____________________________________________________________";
 
@@ -5,6 +7,19 @@ public class Chin {
         System.out.println(LINE);
         System.out.println(" Hello! I'm Chin");
         System.out.println(" What can I do for you?");
+        System.out.println(LINE);
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(LINE);
+            System.out.println(" " + input);
+            System.out.println(LINE);
+        }
+
         System.out.println(LINE);
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println(LINE);
