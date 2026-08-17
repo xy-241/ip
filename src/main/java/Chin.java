cabef9295ec08a4f21cb6ff3e5de8e23a36908e1
Xinyang YU <47915643+xy-241@users.noreply.github.com>
2026-08-17 22:05:00 +0800

    Add Level-3: mark/unmark commands with new Task class (A-Classes)

--- src/main/java/Chin.java
+++ src/main/java/Chin.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Chin {
     private static final String LINE = "____________________________________________________________";
-    private static final String[] tasks = new String[100];
+    private static final Task[] tasks = new Task[100];
     private static int count = 0;
 
     public static void main(String[] args) {
@@ -20,10 +20,20 @@ public class Chin {
             System.out.println(LINE);
             if (input.equals("list")) {
                 for (int i = 0; i < count; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + "." + tasks[i]);
                 }
+            } else if (input.startsWith("mark ")) {
+                int idx = Integer.parseInt(input.substring(5)) - 1;
+                tasks[idx].mark();
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks[idx]);
+            } else if (input.startsWith("unmark ")) {
+                int idx = Integer.parseInt(input.substring(7)) - 1;
+                tasks[idx].unmark();
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks[idx]);
             } else {
-                tasks[count++] = input;
+                tasks[count++] = new Task(input);
                 System.out.println(" added: " + input);
             }
             System.out.println(LINE);
