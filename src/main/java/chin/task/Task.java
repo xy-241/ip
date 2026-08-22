8c07311ec0c7d9329b04312e34da0df0bb3cc2d8
Xinyang YU <47915643+xy-241@users.noreply.github.com>
2026-08-22 13:15:00 +0800

    Add A-JavaDoc: document Chin and Task classes

--- src/main/java/chin/task/Task.java
+++ src/main/java/chin/task/Task.java
@@ -1,22 +1,34 @@
 package chin.task;
 
+/**
+ * Represents a to-do item with a description and a done/not-done state.
+ * Base class for concrete task types like {@link Todo}, {@link Deadline}, and {@link Event}.
+ */
 public class Task {
     protected final String description;
     protected boolean isDone;
 
+    /**
+     * Creates a new task in the not-done state.
+     *
+     * @param description human-readable description
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /** Marks this task as done. */
     public void mark() {
         this.isDone = true;
     }
 
+    /** Marks this task as not done. */
     public void unmark() {
         this.isDone = false;
     }
 
+    /** Returns the {@code [X]}/{@code [ ]} status icon. */
     protected String statusIcon() {
         return "[" + (isDone ? "X" : " ") + "]";
     }
@@ -26,6 +38,7 @@ public class Task {
         return statusIcon() + " " + description;
     }
 
+    /** Returns a pipe-separated line for {@link chin.util.Storage} to persist. */
     public String serialize() {
         return (isDone ? "1" : "0") + " | " + description;
     }
